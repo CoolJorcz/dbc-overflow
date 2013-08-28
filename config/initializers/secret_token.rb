f3be6258e9d762033ad6f4acf4a0112782db1d9d
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 14:20:51 -0500

    Added rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbcOverflow::Application.config.secret_token = '7e8e7153e71469879fc479119b529c36ff9963656663f6f9669426b2b4093e57b60212633c21dd353328c5bcbf22ff9ffafd54a8b61601a2bde8c76cf8aa226a'
