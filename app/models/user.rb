d2c6af01f33d64b03ee4b74bcc2f0cef27935359
Andrew Jorczak <andrew.jorczak@gmail.com>
2013-08-28 15:50:54 -0500

    allowing attr_accessibles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 	has_many :questions
 	has_many :commentables, :through => :comments
 	has_many :voteables, :through => :votes
-	
+
+	attr_accessible :username, :email, :password, :password_confirmation
 	has_secure_password
 
 end
