a69dbe86270500dcfc788a01bf63c0ceae7f4dcf
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-29 13:42:51 -0500

    added answer create action to controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,4 +5,14 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(params[:answer])
+    @answer.user_id = session[:user_id]
+    if @answer.save
+      @question.answers << @answer
+    end
+    redirect_to question_url(@question)
+  end
+
 end
