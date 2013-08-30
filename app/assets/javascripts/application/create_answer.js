601ea1fd9bd6bdb48a1b08ecbfbe7ae612cc1ad3
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 00:09:05 -0500

    finished ajax

--- app/assets/javascripts/application/create_answer.js
+++ app/assets/javascripts/application/create_answer.js
@@ -1,29 +1,3 @@
-// $(function() {
-//   $('#new-answer').hide();
-
-//   $('#add-answer').one("click", function(event){
-//     event.preventDefault();
-//     $('#add-answer').hide();
-//     $('#new-answer').show();
-
-//     $('form').on("submit", function(event){
-//       event.preventDefault();
-
-//       var url = $(this).attr('action');
-//       var data = $('form').serialize();
-
-//       $.post(url, data, function(server_response){
-//         $('#answers').append("<li>" + server_response.answer_text +"</li>")
-//         $('#new-answer').hide();
-//         $('#new-answer #answer_answer_text').val("");
-//       });
-//     });
-//     $('#add-answer').show();
-//   })
-
-  
-// });
-
 $(function() {
   $('#new-answer').hide();
 
