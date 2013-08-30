b45ac871f991bf1815183925196325ea1e697e21
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 00:12:16 -0500

    adding votes and models migrations

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,6 +4,6 @@ describe Answer do
   it { should validate_presence_of(:answer_text) }
   it { should belong_to (:question) }
   it { should belong_to (:user) }
-  it { should have_many(:comments).through(:commentables) }
+  it { should have_many(:commentable).through(:comments) }
   # it { should have_many(:votes).through(:votables) }
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,8 +6,8 @@ describe User do
   it { should validate_presence_of (:email)}
   it { should have_many(:answers) }
   it { should have_many(:questions) }
-  it { should have_many(:comments).through(:commentables) }
-  it { should have_many(:votes).through(:votables) }
+  it { should have_many(:commentable).through(:comments) }
+  it { should have_many(:voteable).through(:votes) }
 
 end
 
